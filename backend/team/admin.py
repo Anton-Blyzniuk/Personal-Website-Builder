b8c4aa999344ad7e316476edb8434ee14cfcadaf
Антон <antonbliznuk71@gmail.com>
2026-03-12 12:20:24 +0000

    add Link model as inline in admin panel for Teammate model.

--- backend/team/admin.py
+++ backend/team/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Teammate, ProjectRole
+
+from .models import Link, ProjectRole, Teammate
+
+
+class LinkInline(admin.TabularInline):
+    model = Link
+    extra = 1
 
 
 @admin.register(ProjectRole)
@@ -7,8 +13,15 @@ class ProjectRoleAdmin(admin.ModelAdmin):
     list_display = ("name",)
     search_fields = ("name",)
 
+
 @admin.register(Teammate)
 class TeammateAdmin(admin.ModelAdmin):
     list_display = ("first_name", "last_name")
     search_fields = ("first_name", "last_name")
+    inlines = [LinkInline]
 
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
